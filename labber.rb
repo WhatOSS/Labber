c873153238dd42828c6da9ffb20cc96b864bc319
th3james <th3james@fastmail.fm>
2014-04-06 18:09:40 +0100

    Make a start on the WarcHtmlReader

--- labber.rb
+++ labber.rb
@@ -1,7 +1,36 @@
+require 'mocha/mini_test'
 require 'minitest/autorun'
 
-class TestTest < Minitest::Test
-  def test_true_succeeds
-    assert true
+class WarcHtmlReader
+  attr_reader :warc
+
+  def initialize(warc)
+    @warc = warc
+  end
+
+  def self.load_from(file_path)
+    reader = self.new(File.read(file_path))
+  end
+end
+
+class WarcHtmlReaderTest < Minitest::Test
+  def test_load_from_reads_in_the_given_file_path
+    file_path = "data/1cy5.mht"
+
+    warc_content = "
+      ------=_FELib_41728.651400463
+      Content-Type: text/html;
+              charset=\"iso-8859-1\"
+      Content-Transfer-Encoding: quoted-printable
+      Content-Location: http://localhost/1.html
+
+        =09<html>
+        =09</html>
+    "
+
+    File.expects(:read).with(file_path).returns(warc_content)
+    WarcHtmlReader.expects(:new).with(warc_content)
+
+    warcHtmlReader = WarcHtmlReader.load_from(file_path)
   end
 end
