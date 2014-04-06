b6ab8d61e68644a54c9e6c79457371a687e1bed6
th3james <th3james@fastmail.fm>
2014-04-06 18:55:16 +0100

    Add ruby-warc, get stuck, give up

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 
 gem "minitest"
 gem 'mocha'
+gem 'warc'
--- labber.rb
+++ labber.rb
@@ -1,3 +1,4 @@
+require 'warc'
 require 'mocha/mini_test'
 require 'minitest/autorun'
 
@@ -9,28 +10,44 @@ class WarcHtmlReader
   end
 
   def self.load_from(file_path)
-    reader = self.new(File.read(file_path))
+    warc = Warc.open_stream(file_path)
+    reader = self.new(warc)
   end
 end
 
 class WarcHtmlReaderTest < Minitest::Test
-  def test_load_from_reads_in_the_given_file_path
-    file_path = "data/1cy5.mht"
+  @@warc_content = "
+    ------=_FELib_41728.651400463
+    Content-Type: text/html;
+            charset=\"iso-8859-1\"
+    Content-Transfer-Encoding: quoted-printable
+    Content-Location: http://localhost/1.html
 
-    warc_content = "
-      ------=_FELib_41728.651400463
-      Content-Type: text/html;
-              charset=\"iso-8859-1\"
-      Content-Transfer-Encoding: quoted-printable
-      Content-Location: http://localhost/1.html
+      =09<html>
+      =09</html>
+    ------
+  "
 
-        =09<html>
-        =09</html>
-    "
+  def test_load_from_loads_a_warm_from_the_given_file_path
+    file_path = "data/1cy5.warc"
+    warc = Warc::Record.new
 
-    File.expects(:read).with(file_path).returns(warc_content)
-    WarcHtmlReader.expects(:new).with(warc_content)
+    Warc.expects(:open_stream).with(file_path).returns(warc)
 
-    warcHtmlReader = WarcHtmlReader.load_from(file_path)
+    warc_html_reader = WarcHtmlReader.load_from(file_path)
+
+    assert_equal warc, warc_html_reader.warc
+  end
+
+  def test_find_returns_correct_html_elements_for_a_selection
+    WarcHtmlReader.new()
   end
 end
+
+file_path = File.expand_path(File.join(File.dirname(__FILE__),"data/test.warc"))
+puts "opening #{file_path}"
+warc_reader = WarcHtmlReader.load_from(file_path)
+
+puts "The Content:"
+puts warc_reader.warc
+puts warc_reader.warc.first
