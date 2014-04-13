367690c889ca9891a466ed4d6d0456b741ec911c
th3james <th3james@fastmail.fm>
2014-04-13 16:36:12 +0100

    Make labber actually write the CSV for the test file

--- bin/labber.rb
+++ bin/labber.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../lib")
 require 'mht'
+require 'csv_builder'
 
 file_path = File.expand_path(File.join(File.dirname(__FILE__),"../data/test.mht"))
 mht = Mht.load_from_file(file_path)
@@ -7,4 +8,4 @@ tables = mht.find_in_html('table')
 
 csv_path = File.expand_path(File.join(File.dirname(__FILE__),"../data/test.csv"))
 csv = CsvBuilder.new(csv_path)
-csv.add_from_html_table(tables[0])
+csv.import_html_table(tables[0])
